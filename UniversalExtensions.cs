f68a186bc92d1bc973b3e3588aea3fdd832e4bd1
Mira <56395159+TheXorog@users.noreply.github.com>
2023-04-24 14:49:29 +0200

    feat: Added CalculatePercentage

--- UniversalExtensions.cs
+++ UniversalExtensions.cs
@@ -618,6 +618,26 @@ public static class UniversalExtensions
         return "#" + R.ToString("X2") + G.ToString("X2") + B.ToString("X2");
     }
 
+
+
+    /// <summary>
+    /// Calculates the percentage of the given 2 values.
+    /// </summary>
+    /// <param name="current">The current value.</param>
+    /// <param name="max">The maximum value.</param>
+    /// <returns>The percentage.</returns>
+    /// <exception cref="ArgumentException"></exception>
+    public static int CalculatePercentage(double current, double max)
+    {
+        if (max == 0)
+            throw new ArgumentException("Max cannot be zero.");
+
+        double percentage = (current / max) * 100;
+        return Convert.ToInt32(percentage);
+    }
+
+
+
     /// <summary>
     /// Shorten a string to the given length
     /// </summary>
